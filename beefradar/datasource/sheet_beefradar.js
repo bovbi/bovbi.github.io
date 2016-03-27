f7d056005302a413222caeaf3569a3d21dda2a2a
fernandommota <mota.fernandomaia@gmail.com>
2016-03-27 08:03:30 -0400

    Atualização de JS da planilha BeefRadar

--- beefradar/datasource/sheet_beefradar.js
+++ beefradar/datasource/sheet_beefradar.js
@@ -1,1 +1,1 @@
-{"data":[]}
+{"data":[{"percentual":20.0,"cor":"red","inico":"20\/03\/2016","fim":"27\/03\/2016","status":"queda"},{"percentual":60.0,"cor":"blue","inico":"20\/03\/2016","fim":"27\/03\/2016","status":"estabilidade"},{"percentual":20.0,"cor":"green","inico":"20\/03\/2016","fim":"27\/03\/2016","status":"alta"}]}
